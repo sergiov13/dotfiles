ba4b9a146a7ae2a1b4fbcb948daea6ae51e47275
svillarroel <sergio.villarroel@newfold.com>
2022-02-16 11:06:51 -0300

    "generated sh file for easier deploy and update of dotfiles"

---
+++ dotfile.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+#colors for text
+GREEN=$(tput setaf 2)
+RESET=$(tput sgr0)
+#vars to store commands issued and args
+COMMAND=""
+SUBCOMMAND=""
+ARGUMENTS=""
+
+
+setCommand() {
+  if [ "$1" == "up" ] || [ "$1" == "down" ];
+  then
+    COMMAND=$1
+  elif [ "$1" != "" ]; then
+    COMMAND="help"
+  fi
+}
+
+setCommand "$@"
+
+if [ "$COMMAND" != "" ]; then
+  if [ "$COMMAND" == "up" ]; then
+  	echo "${GREEN}"
+    echo  "************* Updating Files *************"
+	cp ~/.config/starship.toml .
+    cp ~/.config/kitty/kitty.conf .
+    cp ~/.oh-my-zsh/themes/bureau.zsh-theme .
+    cp ~/.zshrc .
+    echo "${RESET}"
+  elif [ "$COMMAND" == "deploy" ]; then
+  	echo "${GREEN}"
+    echo "************* Deploying Files *************"
+ 	cp starship.toml ~/.config/starship.toml
+    cp kitty.conf ~/.config/kitty/kitty.conf
+    cp bureau.zsh-theme ~/.oh-my-zsh/themes/bureau.zsh-theme
+    cp .zshrc ~/.zshrc
+    echo "${RESET}"
+  fi
+fi
